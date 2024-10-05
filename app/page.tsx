38eff71e364de90e80c5169e1fd6d00afebd2100
Nikandr Surkov <nikandr.dev@gmail.com>
2024-10-05 11:55:15 +0700

    Initial commit 5

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
       <h1 className="text-4xl font-bold mb-8">Telegram Channel Membership Check</h1>
       <input
         type="text"
